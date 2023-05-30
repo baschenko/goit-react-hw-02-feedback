1ad6dceac72fbbbeb9079494ebe6041062a18908
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2023-05-30 18:29:49 +0300

    Update Notification.jsx

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Text } from './Notification.styled';
 
-
 export const Notification = ({ message }) => {
   return <Text>{message}</Text>;
 };
