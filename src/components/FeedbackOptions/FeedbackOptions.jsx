512033abeaea5f0a841256d00289fb20db37419c
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2023-05-30 18:29:40 +0300

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ButtonItem, ButtonList, StatisticsButton } from './FeedbackOptions.styled';
-
+import {
+  ButtonItem,
+  ButtonList,
+  StatisticsButton,
+} from './FeedbackOptions.styled';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <ButtonList>
       {options.map(option => {
-      
-        return <ButtonItem key={option}>
-          <StatisticsButton type="button" data={option} onClick={()=>{onLeaveFeedback(option)} }>
-            {option}
-          </StatisticsButton>
-        </ButtonItem>;}
-      )}
-   </ButtonList>
+        return (
+          <ButtonItem key={option}>
+            <StatisticsButton
+              type="button"
+              data={option}
+              onClick={() => {
+                onLeaveFeedback(option);
+              }}
+            >
+              {option}
+            </StatisticsButton>
+          </ButtonItem>
+        );
+      })}
+    </ButtonList>
   );
 };
 
@@ -22,4 +32,3 @@ FeedbackOptions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string.isRequired),
   onLeaveFeedback: PropTypes.func,
 };
-
